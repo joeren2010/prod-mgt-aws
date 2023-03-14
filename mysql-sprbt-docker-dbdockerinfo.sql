de114f434d5c3ed58d6765f3faece2ebb07c3c8f
joeren2010 <sksports2002@gmail.com>
2023-03-14 06:13:37 -0600

    prep frontend, backend and mysql for aws applications

---
+++ mysql-sprbt-docker-dbdockerinfo.sql
@@ -0,0 +1,19 @@
+-- create database: dockerinfo
+SHOW DATABASES;
+CREATE DATABASE dockerinfo;
+USE dockerinfo;
+SHOW TABLES;
+#DROP DATABASE dockerinfo;
+-- view table: product
+DESC product;
+SELECT * FROM product;
+
+
+-- create database: dockerimage
+SHOW DATABASES;
+USE dockerimage;
+SHOW TABLES;
+#DROP DATABASE dockerimage;
+-- view table: product
+DESC product;
+SELECT * FROM product;
